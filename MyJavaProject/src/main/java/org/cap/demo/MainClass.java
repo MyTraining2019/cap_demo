7387821bf949a1e35ebacb9aa90768dd3f913177
Training2017 <captrainingfeb2016@gmail.com>
2017-05-24 17:19:55 +0530

    display method updated

--- MyJavaProject/src/main/java/org/cap/demo/MainClass.java
+++ MyJavaProject/src/main/java/org/cap/demo/MainClass.java
@@ -4,8 +4,9 @@ public class MainClass {
 
 	public static void display(){
 		System.out.println("Hey!");
-		
+		System.out.println("Hey You are learning GIT!");
 		System.out.println("Greetings! Good After Noon!");
+		System.out.println("Hello World!");
 	}
 	
 	public static void main(String[] args) {
